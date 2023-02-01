cb0fded3d7c022ff9133c9520a09974df68c3fc1
ghaith8064 <83657321+ghaith8064@users.noreply.github.com>
2023-02-02 01:23:42 +0300

    Update main.py

--- main.py
+++ main.py
@@ -14,15 +14,29 @@ from selenium.webdriver import DesiredCapabilities
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
-import time
 
 
 @contextmanager
-def get_selenium_driver(selenium_command_executor):
+def get_selenium_driver(selenium_command_executor, failure_screenshot=None, success_screenshot=None):
+    if failure_screenshot and isfile(failure_screenshot):
+        remove(failure_screenshot)
+    if success_screenshot and isfile(success_screenshot):
+        remove(success_screenshot)
+
     driver = webdriver.Remote(command_executor=selenium_command_executor,
-                              desired_capabilities=DesiredCapabilities.FIREFOX)
+                              desired_capabilities=DesiredCapabilities.CHROME)
     try:
         yield driver
+        if success_screenshot:
+            S = lambda X: driver.execute_script('return document.body.parentNode.scroll' + X)
+            driver.set_window_size(S('Width'), S('Height'))
+            driver.find_element_by_tag_name('body').screenshot(success_screenshot)
+    except:
+        if failure_screenshot:
+            S = lambda X: driver.execute_script('return document.body.parentNode.scroll' + X)
+            driver.set_window_size(S('Width'), S('Height'))
+            driver.find_element_by_tag_name('body').screenshot(failure_screenshot)
+        raise
     finally:
         driver.delete_all_cookies()
         driver.quit()
@@ -30,17 +44,18 @@ def get_selenium_driver(selenium_command_executor):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Looks for available appointments in TLSContact.')
-    parser.add_argument('--tls-application-reference', type=str, help='TLSContact application reference', required=True)
-    parser.add_argument('--login', type=str, help='TLSContact login', required=True)
-    parser.add_argument('--password', type=str, help='TLSContact password', required=True)
-    parser.add_argument('--telegram-bot-token', type=str, help='Telegram bot`s token', required=True)
-    parser.add_argument('--telegram-chat-id', type=int,
+    parser.add_argument('M1194289', type=str, help='TLSContact application reference', required=True)
+    parser.add_argument('eltun8064@gmail.com', type=str, help='TLSContact login', required=True)
+    parser.add_argument('AaBb1230992-', type=str, help='TLSContact password', required=True)
+    parser.add_argument('6016195930:AAHu9n_2ESsuva-v_xkdCdDW2f56G7JdVl4', type=str, help='Telegram bot`s token',
+                        required=True)
+    parser.add_argument('ghaithashi', type=int,
                         help='Telegram chat id to send notifications to (you can use @userinfobot to get your id)',
                         required=True)
-    parser.add_argument('--search-before', type=str,
+    parser.add_argument('2023-05-01', type=str,
                         help='Notify when appointment is available before this date, e.g. 2019-10-20',
                         required=True)
-    parser.add_argument('--delay', type=int,
+    parser.add_argument('1000', type=int,
                         help='Delay in seconds before retries (recommended value is 5400 seconds or 1.5 hours)',
                         default=5400)
     parser.add_argument('--selenium-executor', type=str,
@@ -61,16 +76,16 @@ if __name__ == '__main__':
             break
 
         try:
-            retry = False
             logging.info('Starting...')
-            with get_selenium_driver(args.selenium_executor) as driver:
-                driver.get('https://fr.tlscontact.com/gb/LON/login.php')
+            with get_selenium_driver(args.selenium_executor, './static/tls_contact_error.png',
+                                     './static/tls_contact_success.png') as driver:
+                driver.get('https://it.tlscontact.com/by/msq/login.php')
                 WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, 'email')))
-                driver.get('https://fr.tlscontact.com/gb/LON/login.php')
+                driver.get('https://it.tlscontact.com/by/msq/login.php')
                 email = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, 'email')))
                 email.send_keys(args.login)
                 driver.find_element_by_id('pwd').send_keys(args.password)
-                btn = driver.find_element_by_xpath('//input[@type="button"]')
+                btn = driver.find_element_by_xpath('//input[@type="submit"]')
                 btn.click()
                 WebDriverWait(driver, 15).until(EC.staleness_of(btn))
 
@@ -89,61 +104,36 @@ if __name__ == '__main__':
                     pass
 
                 if not has_error:
-                    try:
-                        driver.find_element_by_id('pwd')
-                        logging.error('We`re still on the log in page')
-                        retry = True
-
-                        if args.debug:
-                            with open("./static/page_%d.html" % (time.time(),), "wb") as fh:
-                                fh.write(driver.page_source.encode('utf-8'))
-
-                        continue
-                    except NoSuchElementException:
-                        pass
-
+                    S = lambda X: driver.execute_script('return document.body.parentNode.scroll' + X)
+                    driver.set_window_size(S('Width'), S('Height'))
+                    driver.find_element_by_tag_name('body').screenshot('./static/after_login.png')
                     WebDriverWait(driver, 10).until(
-                        EC.visibility_of_element_located(
-                            (By.XPATH, '//*[text()="%s"]' % args.tls_application_reference)))
-
-                    if args.debug:
-                        with open("./static/page_%d.html" % (time.time(),), "wb") as fh:
-                            fh.write(driver.page_source.encode('utf-8'))
-
+                            EC.visibility_of_element_located(
+                                    (By.XPATH, '//*[text()="%s"]' % args.tls_application_reference)))
                     first_appointment = driver.find_element_by_xpath(
-                        '//div[@class="inner_timeslot"][a[@class="appt-table-btn dispo"]]/span[@class="appt-table-d"]')
-                    first_date = re.sub(r'^.*?(\w+\s+\d+).*$', r'\1', first_appointment.text, 0, re.DOTALL)
-                    date = datetime.datetime.strptime(str(datetime.date.today().year) +
-                                                      ' ' + first_date, '%Y %B %d').date()
+                            '//div[@class="inner_timeslot"][a[@class="appt-table-btn dispo"]]/span[@class="appt-table-d"]')
+                    first_date = re.sub(r'^.*?(\w+\s+\d+)\w+.*$', r'\1', first_appointment.text, 0, re.DOTALL)
+                    date = datetime.datetime.strptime(str(datetime.date.today().year) + ' ' + first_date, '%Y %B %d').date()
 
                     if date < datetime.date.today():
-                        date = datetime.datetime.strptime(
-                            str(datetime.date.today().year + 1) + ' ' + first_date, '%Y %B %d').date()
+                        date = datetime.datetime.strptime(str(datetime.date.today().year + 1) + ' ' + first_date, '%Y %B %d').date()
 
                     logging.info('Got date ' + date.strftime('%Y-%m-%d'))
 
                     if date < datetime.datetime.strptime(args.search_before, '%Y-%m-%d').date():
-                        bot.send_message(chat_id=args.telegram_chat_id, text='New date found! %s' %
-                                         date.strftime('%Y-%m-%d'))
-                        bot.send_message(chat_id=args.telegram_chat_id, text='Current cookies: %s' %
-                                         driver.get_cookies())
+                        bot.send_message(chat_id=args.telegram_chat_id, text='New date found! %s' % date.strftime('%Y-%m-%d'))
+                        bot.send_message(chat_id=args.telegram_chat_id, text='Current cookies: %s' % driver.get_cookies())
                     else:
                         bot.send_message(chat_id=args.telegram_chat_id,
                                          text='No new dates, earliest available is %s' % date.strftime('%Y-%m-%d'))
         except KeyboardInterrupt:
             logging.info('Terminating...')
-            args.once = True
             break
         except Exception as e:
-            if args.debug:
-                with open("./static/page_%d.html" % (time.time(),), "wb") as fh:
-                    fh.write(driver.page_source.encode('utf-8'))
-
             logging.exception('Got error')
             bot.send_message(chat_id=args.telegram_chat_id, text='Got an exception! %s' % e)
         finally:
             logging.info('Done')
             if args.once:
                 break
-            if not retry:
-                sleep(args.delay)
+            sleep(args.delay)
